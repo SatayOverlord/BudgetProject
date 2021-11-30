e3b4a0fa608a179c7b4afe86557bf0256433f2e1
Fabian Steen <fabian.steen.1997@gmail.com>
2021-11-30 19:55:30 +0100

    budget: Add unique ID generator

---
+++ Budget/Utils/Src/generateUniqueId.py
@@ -0,0 +1,7 @@
+import datetime as dt
+import uuid
+
+def generate_unique_id():
+    time = dt.datetime.now().strftime('%y%m%d-%H%M%S-%f')
+    random_id = str(uuid.uuid4())
+    return '-'.join([time, random_id])
