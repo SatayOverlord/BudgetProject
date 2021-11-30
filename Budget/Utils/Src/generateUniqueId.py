0cab0dbc05a56b1b183cbed7c46e4a84ac6e6435
Fabian Steen <fabian.steen.1997@gmail.com>
2021-11-30 20:06:41 +0100

    budget: Update generate_unique_id to return correct type

--- Budget/Utils/Src/generateUniqueId.py
+++ Budget/Utils/Src/generateUniqueId.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import uuid
 
-def generate_unique_id() -> None:
+def generate_unique_id() -> str:
     time = dt.datetime.now().strftime('%y%m%d-%H%M%S-%f')
     random_id = str(uuid.uuid4())
     return '-'.join([time, random_id])
