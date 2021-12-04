c1acc56198b961775855b26e82fa3e14d19f65ad
Fabian Steen <fabian.steen.1997@gmail.com>
2021-12-04 15:42:25 +0100

    budget: Update Account component

--- Budget/Components/Account/Src/Account.py
+++ Budget/Components/Account/Src/Account.py
@@ -1,17 +1,31 @@
+import datetime as dt
+
+
 from .TransactionData import TransactionData
 
 
 class Account:
     def __init__(self):
-        self._balance: float = 0
+        self._balance: float = 0.0
         self._history: list = []
 
-    def deposit(self, amount: float, description: str, tags: list = []) -> None:
-        # Should date argument be added to deposit?
-        pass
+    def deposit(self, amount: float, date: dt.date, description: str, tags: list = []) -> None:
+        if type(amount) is not float:
+            amount = float(amount)
+        tags.append("deposit")
+        data: TransactionData = TransactionData(amount, date, description, tags)
 
-    def withdraw(self, amount: float, description: str, tags: list = []) -> None:
-        pass
+        self._balance += amount
+        self._history.append(data)
+
+    def withdraw(self, amount: float, date: dt.date, description: str, tags: list = []) -> None:
+        if type(amount) is not float:
+            amount = float(amount)
+        tags.append("withdraw")
+        data: TransactionData = TransactionData(amount, date, description, tags)
+
+        self._balance -= amount
+        self._history.append(data)
 
     def balance(self) -> float:
         return self._balance
