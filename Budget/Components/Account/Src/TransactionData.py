cdb112825b2218e538455ca624adceaad1c26a62
Fabian Steen <fabian.steen.1997@gmail.com>
2021-12-04 15:41:31 +0100

    budget: Update TransactionData and add test

--- Budget/Components/Account/Src/TransactionData.py
+++ Budget/Components/Account/Src/TransactionData.py
@@ -5,6 +5,8 @@ from Budget.Utils import generate_unique_id
 
 class TransactionData:
     def __init__(self, amount: float, date: dt.date, description: str, tags: list):
+        if type(amount) is not float:
+            amount = float(amount)
         self._amount: float = amount
         self._date: dt.date = date
         self._description: str = description
@@ -12,41 +14,59 @@ class TransactionData:
 
         self._id: str = generate_unique_id()
 
+
     def get_amount(self) -> float:
         return self._amount
+
 
     def get_date(self) -> dt.date:
         return self._date
 
+
     def get_description(self) -> str:
         return self._description
+
 
     def get_tags(self) -> list:
         return self._tags
 
+
     def get_id(self) -> str:
-        return self._amount
+        return self._id
+
 
     def set_amount(self, amount: float) -> None:
+        if type(amount) is not float:
+            amount = float(amount)
         self._amount = amount
+
 
     def set_date(self, date: dt.date) -> None:
         self._date = date
 
+
     def set_description(self, description: str) -> None:
         self._description = description
+
 
     def clear_tags(self) -> None:
         self._tags = []
 
-    def remove_tag(self, tag: str) -> None:
+
+    def remove_tag(self, tag: str) -> bool:
         try:
             self._tags.remove(tag)
         except ValueError:
-            print(f"Object has no tag \"{tag}\"")
+            print("Object has no tag \"%s\"" % tag)
+            return False
 
-    def add_tag(self, tag: str) -> None:
+        return True
+
+
+    def add_tag(self, tag: str) -> bool:
         if tag in self._tags:
-            print(f"Tag \"{tag}\" already exist")
+            print("Tag \"%s\" already exist" % tag)
+            return False
         else:
             self._tags.append(tag)
+            return True
---
+++ Budget/Components/Account/Test/TestTransactionData.py
@@ -0,0 +1,116 @@
+import datetime as dt
+import unittest
+
+from Budget.Components.Account.Src.TransactionData import TransactionData
+
+
+class TestTransactionData(unittest.TestCase):
+    def setUp(self):
+        self.amount = 25000.0
+        self.date = dt.date(2021, 12, 4)
+        self.description = "Lön"
+        self.tag = "default"
+
+        self.new_amount = 2000.0
+        self.new_date = dt.date(2021, 11, 12)
+        self.new_description = "Övrigt"
+        self.new_tag = "change"
+
+
+    def testCreateTransactionData(self):
+        tags = [self.tag]
+
+        # ### Create TransactionData ###
+        td = TransactionData(self.amount,
+                             self.date,
+                             self.description,
+                             tags)
+
+        self.assertEqual(td.get_amount(), self.amount)
+        self.assertEqual(td.get_date(), self.date)
+        self.assertEqual(td.get_description(), self.description)
+        self.assertEqual(td.get_tags(), tags)
+
+        id = td.get_id()
+        id_parts = id.split('-')
+        id_length = sum(map(len, id_parts))
+
+        self.assertEqual(id_length, 50)
+
+
+    def testUpdateTransactionDataFields(self):
+        tags = [self.tag]
+
+        # ### Create TransactionData ###
+        td = TransactionData(self.amount,
+                             self.date,
+                             self.description,
+                             tags)
+
+        id = td.get_id()
+
+        # ### Update TransactionData using setters ###
+        td.set_amount(self.new_amount)
+        td.set_date(self.new_date)
+        td.set_description(self.new_description)
+
+        self.assertEqual(td.get_amount(), self.new_amount)
+        self.assertEqual(td.get_date(), self.new_date)
+        self.assertEqual(td.get_description(), self.new_description)
+        self.assertEqual(td.get_id(), id)
+
+
+    def testchangeTransactionDataTags(self):
+        tags = [self.tag]
+
+        # ### Create TransactionData ###
+        td = TransactionData(self.amount,
+                             self.date,
+                             self.description,
+                             tags)
+
+        id = td.get_id()
+
+        self.assertEqual(td.get_tags(), tags)
+
+        # ### Update tags ###
+        tags.append(self.new_tag)
+        td.add_tag(self.new_tag)
+
+        self.assertEqual(td.get_tags(), tags)
+
+        # ### Clear tags ###
+        td.clear_tags()
+
+        self.assertEqual(td.get_tags(), [])
+
+        # ### Test remove_tag ###
+
+        # Add new tag to empty list
+        self.assertTrue(td.add_tag(self.tag))
+        # Add new tag to list
+        self.assertTrue(td.add_tag(self.new_tag))
+        # Add already existing tag to list
+        self.assertFalse(td.add_tag(self.new_tag))
+        # Check that all tags added correctly
+        self.assertEqual(td.get_tags(), tags)
+        # Remove first tag from list
+        self.assertTrue(td.remove_tag(self.tag))
+        # Check that correct tag was removed
+        self.assertEqual(td.get_tags(), [self.new_tag])
+        # Remove second tag
+        self.assertTrue(td.remove_tag(self.new_tag))
+        # Check that the tag was removed
+        self.assertEqual(td.get_tags(), [])
+        # Try to remove tag from empty list
+        self.assertFalse(td.remove_tag(self.tag))
+
+        # Check that nothing else changed
+        self.assertEqual(td.get_amount(), self.amount)
+        self.assertEqual(td.get_date(), self.date)
+        self.assertEqual(td.get_description(), self.description)
+        self.assertEqual(td.get_id(), id)
+
+
+if __name__ == '__main__':
+    unittest.main()
